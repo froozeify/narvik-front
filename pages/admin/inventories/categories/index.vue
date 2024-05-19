070aab6b70aa8135476adf372c320d62cd8dea6f
Benoit VIGNAL <git@benoit-vignal.fr>
2024-05-19 23:58:36 +0200

    Switch to div and not table

--- pages/admin/inventories/categories/index.vue
+++ pages/admin/inventories/categories/index.vue
@@ -12,30 +12,6 @@
 
   const selectedCategory: Ref<{id: number, weight: number, name: string} | null> = ref(null)
 
-  const page = ref(1);
-  const itemsPerPage = ref(10);
-  const sort = ref({
-    column: 'weight',
-    direction: 'asc'
-  });
-
-  const columns = [
-    // {
-    //   key: 'weight',
-    //   label: 'Ordre',
-    //   // sortable: true,
-    //   class: 'w-12'
-    // },
-    {
-      key: 'name',
-      label: 'Nom',
-      class: 'w-full'
-    },
-    {
-      key: 'actions',
-    }
-  ]
-
   const categories = ref([
     {
       id: 1,
@@ -60,41 +36,41 @@
 
   function move(row: any, modifier: number) {
     // TODO: Change this for api call (so they are all moved cleanly in bo)
+    // row.weight += modifier
+
+    const sortedCategories = categories.value.sort((a, b) => (a.weight > b.weight ? 1 : -1))
+    const matchToExchange = sortedCategories.find( value => value.weight == row.weight + modifier )
+
+    if (!matchToExchange) return;
+
+    matchToExchange.weight = row.weight
     row.weight += modifier
   }
+
 </script>
 
 <template>
   <div class="flex flex-col-reverse lg:flex-row gap-4">
     <UCard class="lg:w-2/3 shrink-0">
-      <UTable
-        class="w-full"
-        :sort="sort"
-        :columns="columns"
-        :rows="categories"
-        @select="rowClicked">
-        <template #empty-state>
-          <div class="flex flex-col items-center justify-center py-6 gap-3">
-            <span class="italic text-sm">Aucune catégories.</span>
-          </div>
-        </template>
+      <div class="flex flex-col flex-wrap">
+        <div v-for="(category, index) in categories.sort((a, b) => (a.weight > b.weight ? 1 : -1))"
+             class="flex-1 flex gap-4 cursor-pointer hover:bg-white dark:hover:bg-gray-800/50 transition-all px-1 py-2 rounded-md items-center"
+             @click="rowClicked(category)"
+        >
+          <div class="flex-1">{{ category.name }}</div>
 
-        <template #name-data="{ row }">
-          {{ row.name }}
-        </template>
 
-        <template #actions-data="{ row }">
+          <!-- Actions -->
           <div class="text-xs flex flex-col gap-0.5">
-            <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-              <UIcon name="i-heroicons-chevron-up" @click="move(row, -1)"/>
+            <div v-if="index != 0" class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
+              <UIcon name="i-heroicons-chevron-up" @click.stop="move(category, -1)"/>
             </div>
-            <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-              <UIcon name="i-heroicons-chevron-down" @click="move(row, 1)"/>
+            <div v-if="index != categories.length - 1" class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
+              <UIcon name="i-heroicons-chevron-down" @click.stop="move(category, 1)"/>
             </div>
           </div>
-        </template>
-
-      </UTable>
+        </div>
+      </div>
     </UCard>
 
     <div v-if="selectedCategory" class="w-full h-fit lg:sticky lg:top-20 max-h-[calc(100vh-6rem)] flex flex-col gap-4">
