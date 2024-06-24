0ca282d52fa82b5fa188385b7a5563079432ba9b
Benoit VIGNAL <git@benoit-vignal.fr>
2024-06-24 19:38:06 +0200

    Update header menu ui

--- components/HeaderNavbar.vue
+++ components/HeaderNavbar.vue
@@ -8,10 +8,10 @@
   const appConfigStore = useAppConfigStore();
 
   const isDark = computed({
-    get () {
+    get() {
       return colorMode.value === 'dark'
     },
-    set () {
+    set() {
       colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
     }
   });
@@ -21,65 +21,82 @@
   const isBadger = selfStore.isBadger()
   const isSupervisor = selfStore.hasSupervisorRole()
 
-  const siteLogo: Ref<Image|null> = appConfigStore.getLogo()
+  const siteLogo: Ref<Image | null> = appConfigStore.getLogo()
+
+  const rightMenu = [
+    [{
+      label: !isBadger ? selfStore.member?.fullName : 'Pointeuse',
+      avatar: {
+        icon: 'i-heroicons-user',
+        size: 'xs',
+        src: selfStore.member?.profileImageBase64
+      },
+      to: !isBadger ? "/self" : ''
+    }], [{
+      slot: 'darkMode',
+      label: 'Thème',
+      click: () => {
+        isDark.value = !isDark.value
+      }
+    }, {
+      label: 'Déconnexion',
+      icon: 'i-heroicons-arrow-right-start-on-rectangle-20-solid',
+      click: () => {
+        selfStore.logout()
+      }
+    }]
+  ]
 </script>
 
 <template>
-  <header class="backdrop-blur -mb-px sticky top-0 z-50 border-b border-gray-200 dark:border-gray-800 h-16 print:hidden">
+  <header
+    class="backdrop-blur -mb-px sticky top-0 z-50 border-b border-gray-200 dark:border-gray-800 h-16 print:hidden">
     <nav class="container mx-auto p-4 flex justify-between h-full">
       <ul class="flex gap-4">
         <li>
           <NuxtLink to="/" class="flex align-middle">
             <span v-if="!siteLogo">Accueil</span>
             <UTooltip v-else
-              text="Accueil"
+                      text="Accueil"
             >
-              <img :src="siteLogo.base64" class="w-7" />
+              <img :src="siteLogo.base64" class="w-7"/>
             </UTooltip>
           </NuxtLink>
         </li>
-        <li v-if="isSupervisor"><UButton to="/admin/sales" icon="i-heroicons-shopping-cart" variant="ghost" color="gray">Vente</UButton></li>
-      </ul>
-      <ul class="flex gap-4">
-        <li v-if="isSupervisor"><UButton to="/admin" icon="i-heroicons-key" variant="ghost" color="gray">Administration</UButton></li>
-        <li>
-          <UTooltip text="Mode clair/sombre">
-            <UButton
-                :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-                color="gray"
-                variant="ghost"
-                aria-label="Theme"
-                @click="isDark = !isDark"
-            />
-          </UTooltip>
+        <li v-if="isSupervisor">
+          <UButton to="/admin/sales" icon="i-heroicons-shopping-cart" variant="ghost" color="gray">Vente</UButton>
         </li>
-        <li v-if="!isBadger">
+      </ul>
+      <div class="flex gap-4">
+        <div v-if="isSupervisor">
+          <UButton to="/admin" icon="i-heroicons-key" variant="ghost" color="gray">Administration</UButton>
+        </div>
+        <UDropdown :items="rightMenu">
           <UButton
-              icon="i-heroicons-user-circle-solid"
-              color="gray"
-              variant="ghost"
-              to="/self"
+            variant="ghost"
+            color="gray"
+            trailing-icon="i-heroicons-cog-6-tooth"
           />
-        </li>
-        <li>
-          <UTooltip text="Déconnexion">
-            <UButton
-                icon="i-heroicons-arrow-right-start-on-rectangle-20-solid"
-                color="gray"
-                variant="ghost"
-                aria-label="Déconnexion"
-                @click="selfStore.logout()"
-            />
-          </UTooltip>
-        </li>
-      </ul>
+
+          <template #darkMode>
+              <UIcon
+                :name="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+                class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-gray-500"
+                />
+              <span>
+                {{ isDark ? 'Thème sombre' : 'Thème clair' }}
+              </span>
+          </template>
+
+        </UDropdown>
+      </div>
     </nav>
   </header>
 </template>
 
 <style scoped lang="scss">
-  li {
-    display: flex;
-    align-items: center;
-  }
+li {
+  display: flex;
+  align-items: center;
+}
 </style>
