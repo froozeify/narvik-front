a8f0ca2f7482848d2c5ee4f5f030d20e2539764e
Benoit VIGNAL <git@benoit-vignal.fr>
2024-07-16 23:52:26 +0200

    Add pointer on generic upDown

--- components/Generic/GenericStackedUpDown.vue
+++ components/Generic/GenericStackedUpDown.vue
@@ -15,10 +15,10 @@ const emit = defineEmits([
 <template>
   <div class="text-xs flex flex-col gap-0.5">
     <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-      <UIcon name="i-heroicons-chevron-up" @click.stop="emit('changed', props.step)"/>
+      <UIcon name="i-heroicons-chevron-up" class="cursor-pointer" @click.stop="emit('changed', props.step)"/>
     </div>
     <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-      <UIcon name="i-heroicons-chevron-down" @click.stop="emit('changed', - props.step)"/>
+      <UIcon name="i-heroicons-chevron-down" class="cursor-pointer" @click.stop="emit('changed', - props.step)"/>
     </div>
   </div>
 </template>
