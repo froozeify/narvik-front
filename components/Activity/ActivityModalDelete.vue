5d075302c39d70d8cb08b1fc7d88079bea414c58
Benoit VIGNAL <git@benoit-vignal.fr>
2024-07-22 10:16:28 +0200

    #42: Refresh activities page

---
+++ components/Activity/ActivityModalDelete.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const props = defineProps(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+  }
+)
+
+const emit = defineEmits(['delete'])
+
+</script>
+
+<template>
+  <ModalWithActions :title="props.title">
+
+    <UAlert
+      class="my-4"
+      color="orange"
+      variant="soft"
+      title="La suppression d'une activité, supprimera aussi toutes les activités des membres liées."
+      description="Il conseillé de faire un migration à la place."
+    />
+
+    <template #actions>
+      <UButton
+        @click="emit('delete')"
+        color="red"
+      >
+        Valider la suppression
+      </UButton>
+    </template>
+  </ModalWithActions>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ components/Activity/ActivityModalMigrate.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+import type {PropType} from "vue";
+import type {Activity} from "~/types/activity";
+
+const props = defineProps(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    activities: {
+      type: Object as PropType<Activity[]>,
+      required: true
+    }
+  }
+)
+
+const emit = defineEmits(['migrate'])
+
+const migrationTarget: Ref<string|undefined> = ref(undefined)
+
+</script>
+
+<template>
+  <ModalWithActions :title="props.title">
+
+    <UAlert
+      class="my-4"
+      color="yellow"
+      variant="soft"
+      title="Une fois la migration effectuée, l'activité sera supprimée."
+    />
+
+    <UFormGroup class="mb-4" label="Activité cible">
+      <USelect required v-model="migrationTarget" :options="props.activities" option-attribute="name" value-attribute="id" />
+    </UFormGroup>
+
+    <template #actions>
+      <UButton
+        :disabled="!migrationTarget"
+        @click="emit('migrate', migrationTarget)"
+        color="orange"
+      >
+        Migrer
+      </UButton>
+    </template>
+  </ModalWithActions>
+</template>
+
+<style scoped lang="scss">
+
+</style>
