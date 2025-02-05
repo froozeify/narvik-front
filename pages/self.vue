ae6e562b993c65bc72f40e4683348a24d9f2e2cb
Benoit VIGNAL <git@benoit-vignal.fr>
2025-02-05 11:23:18 +0100

    Update self no account message display

--- pages/self.vue
+++ pages/self.vue
@@ -11,13 +11,16 @@
   })
 
   const selfStore = useSelfUserStore()
-  const self: Ref<Member | undefined> = ref(selfStore.member)
-
+  const { member } = storeToRefs(selfStore)
   selfStore.refresh()
 </script>
 
 <template>
   <div>
-    <MemberDetails v-if="self" :member="self" />
+    <MemberDetails v-if="member" :member="member" />
+    <UCard v-else class="text-center">
+      <div class="text-xl font-bold">Compte liée à aucun club</div>
+      <p>Veuillez vous assurer que l'adresse mail du compte correspond à celle enregistré auprès de votre club</p>
+    </UCard>
   </div>
 </template>
