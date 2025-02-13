9e35b10f06c1a6bb85d1a62be5f8d14037cbf719
Benoit VIGNAL <git@benoit-vignal.fr>
2025-02-13 22:48:17 +0100

    Add external presence improt

--- composables/api/query/clubDependent/plugin/presence/ExternalPresenceQuery.ts
+++ composables/api/query/clubDependent/plugin/presence/ExternalPresenceQuery.ts
@@ -1,13 +1,32 @@
 import {AbstractQuery} from "~/composables/api/query/AbstractQuery";
 import type {FetchAllData} from "~/types/api/api";
-import {useFetchList} from "~/composables/api/api";
+import {useFetchList, useGetCsv, useUploadFile} from "~/composables/api/api";
 import type {ExternalPresence} from "~/types/api/item/clubDependent/plugin/presence/externalPresence";
 import {AbstractClubDependentQuery} from "~/composables/api/query/AbstractClubDependentQuery";
 
 export default class ExternalPresenceQuery extends AbstractClubDependentQuery<ExternalPresence, ExternalPresence> {
-    rootPath = "external-presences";
+  rootPath = "external-presences";
 
-    async getPresentToday(): Promise<FetchAllData<ExternalPresence>> {
-        return useFetchList<ExternalPresence>(this.getRootUrl() + "/-/today");
+  async getAllCsv(urlParams?: URLSearchParams) {
+    let url = `${this.getRootUrl()}.csv`;
+
+    if (!urlParams) {
+      urlParams = new URLSearchParams()
     }
+    if (!urlParams.has('pagination')) {
+      urlParams.append('pagination', 'false')
+    }
+
+    url += '?' + urlParams.toString()
+
+    return useGetCsv(url)
+  }
+
+  async getPresentToday(): Promise<FetchAllData<ExternalPresence>> {
+    return useFetchList<ExternalPresence>(this.getRootUrl() + "/-/today");
+  }
+
+  async importFromCsv(formData: FormData) {
+    return useUploadFile(this.getRootUrl() + "/-/from-csv", formData)
+  }
 }
--- pages/admin/imports/presences.vue
+++ pages/admin/imports/presences.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
   import MemberPresenceQuery from "~/composables/api/query/clubDependent/plugin/presence/MemberPresenceQuery";
   import {displayFileErrorToast, displayFileSuccessToast, getFileFormDataFromUInputChangeEvent} from "~/utils/file";
+  import ExternalPresenceQuery from "~/composables/api/query/clubDependent/plugin/presence/ExternalPresenceQuery";
 
   definePageMeta({
     layout: "admin"
@@ -19,8 +20,9 @@
   })
 
   const memberPresenceQuery = new MemberPresenceQuery()
+  const externalPresenceQuery = new ExternalPresenceQuery()
 
-  async function getFileObject(event: any) {
+  async function importMemberPresences(event: any) {
     const formData = getFileFormDataFromUInputChangeEvent(event);
 
     if (!formData) {
@@ -39,42 +41,92 @@
     displayFileSuccessToast()
   }
 
+  async function importExternalPresences(event: any) {
+    const formData = getFileFormDataFromUInputChangeEvent(event);
+
+    if (!formData) {
+      return;
+    }
+
+    fileUploading.value = true
+    const {created, error} = await externalPresenceQuery.importFromCsv(formData)
+    fileUploading.value = false
+
+    if (error) {
+      return displayFileErrorToast(error.message)
+    }
+
+    apiUploadResponse.value = created
+    displayFileSuccessToast()
+  }
+
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col gap-4">
+    <MetricAdminImportBatches />
 
+    <UAlert
+      icon="i-heroicons-megaphone"
+      color="orange"
+      title="Veuillez-vous assurer que tous les membres sont bien importés avant."
+      description="L'import ne créera pas de nouveaux membres ni activités."
+    />
 
     <UCard>
-      <MetricAdminImportBatches />
-
-      <UAlert
-        icon="i-heroicons-megaphone"
-        color="orange"
-        title="Veuillez-vous assurer que tous les membres sont bien importés avant."
-        description="L'import ne créera pas de nouveaux membres ni activités."
-      />
-      <p class="mt-4">L'import ce fait grâce au fichier csv exporté depuis l'historique de présence.</p>
+      <p>L'import ce fait grâce au fichier csv exporté depuis l'historique de présence.</p>
       <p>Celui-ci doit obligatoirement être au format csv.</p>
 
-      <p class="mt-4 mb-2">Example de csv possible</p>
-      <pre class=" w-full text-xs inline-flex text-left items-center space-x-4 bg-gray-800 text-white rounded-lg p-4 overflow-x-auto whitespace-pre">
+      <div class="grid grid-cols-11 my-4">
+        <div class="col-span-5 space-y-2">
+          <p class="font-bold">Membres</p>
+          <UInput
+            :loading="fileUploading"
+            :disabled="fileUploading"
+            class="my-4"
+            type="file"
+            accept="text/csv"
+            icon="i-heroicons-document-text"
+            v-model="state.file"
+            @change="importMemberPresences"
+          />
+
+          <p class="">Example</p>
+
+          <pre class=" w-full text-xs inline-flex text-left items-center space-x-4 bg-gray-800 text-white rounded-lg p-4 overflow-x-auto whitespace-pre">
 member.licence,date,activities.0.name,activities.1.name
 01234578,2018-08-03T00:00:00+00:00,50M - Toutes armes,Non existant
 45678412,2018-08-03T00:00:00+00:00,25M,
 45678412,2018-08-04T00:00:00+00:00,50M - Toutes armes,
 45671134,2018-08-03T00:00:00+00:00,Bureau,</pre>
+        </div>
 
-      <UInput
-        :loading="fileUploading"
-        :disabled="fileUploading"
-        class="my-4"
-        type="file"
-        accept="text/csv"
-        icon="i-heroicons-document-text"
-        v-model="state.file"
-        @change="getFileObject"
-      />
+        <UDivider class="col-span-1" orientation="vertical" />
+
+        <div class="col-span-5 space-y-2">
+          <p class="font-bold">Présences externe</p>
+          <UInput
+            :loading="fileUploading"
+            :disabled="fileUploading"
+            class="my-4"
+            type="file"
+            accept="text/csv"
+            icon="i-heroicons-document-text"
+            v-model="state.file"
+            @change="importExternalPresences"
+          />
+
+          <p class="">Example</p>
+
+          <pre class=" w-full text-xs inline-flex text-left items-center space-x-4 bg-gray-800 text-white rounded-lg p-4 overflow-x-auto whitespace-pre">
+licence,firstname,lastname,date,activities.0.name,activities.1.name
+01234578,Jean,NOM,2018-08-03T00:00:00+00:00,50M - Toutes armes,Non existant
+,Jean,NOM2,2018-08-03T00:00:00+00:00,25M,
+,Jean,NOM3,2018-08-04T00:00:00+00:00,50M - Toutes armes,
+,Jean,NOM4,2018-08-03T00:00:00+00:00,Bureau,</pre>
+        </div>
+      </div>
+
 
       <pre v-if="apiUploadResponse" class="mb-4 w-full text-xs inline-flex text-left items-center space-x-4 bg-gray-800 text-white rounded-lg p-4 overflow-x-auto whitespace-pre">{{apiUploadResponse}}</pre>
 
