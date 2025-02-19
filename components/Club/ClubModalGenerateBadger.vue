6cd6194b7b498265e48a5ef5ae244053eb9fdd44
Benoit VIGNAL <git@benoit-vignal.fr>
2025-02-19 18:19:21 +0100

    Add badger regenrate link

---
+++ components/Club/ClubModalGenerateBadger.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+import ClubQuery from "~/composables/api/query/ClubQuery";
+
+const emit = defineEmits(['generated'])
+
+async function generateBadger() {
+  const clubQuery = new ClubQuery()
+  const { updated } = await clubQuery.generateBadger()
+  if (updated) {
+    emit("generated", updated)
+  }
+}
+
+</script>
+
+<template>
+  <ModalWithActions title="Génération d'un nouveau lien">
+
+    <UAlert
+      icon="i-heroicons-megaphone"
+      class="my-4"
+      color="red"
+      variant="soft"
+      title="Une fois la génération effectué, l'ancien lien ne fonctionnera plus."
+    />
+
+    <template #actions>
+      <UButton
+        @click="generateBadger()"
+      >
+        Générer
+      </UButton>
+    </template>
+  </ModalWithActions>
+</template>
+
+<style scoped lang="scss">
+
+</style>
