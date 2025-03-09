959583a9fe30f70833913ac8d7dc50034b5b7569
Benoit VIGNAL <git@benoit-vignal.fr>
2025-03-09 16:49:15 +0100

    Update member details ui

---
+++ components/Member/MemberDetailsPersonnalInfo.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: false
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+    required: false
+  }
+});
+
+</script>
+
+<template>
+  <div class="flex gap-2 items-center">
+    <UIcon :name="props.icon" />
+
+    <ULink v-if="props.to"
+           :to="props.to"
+           class="text-primary-500 hover:text-primary-600 hover:underline dark:text-primary-400 dark:hover:text-primary-500 underline-offset-4"
+    >
+      {{ props.label ?? 'Non défini' }}
+    </ULink>
+    <ULink v-else disabled>
+      {{ props.label ?? 'Non défini' }}
+    </ULink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
