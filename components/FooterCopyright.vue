168bf2733b14b98661fe8d9241e60b076410662b
Benoit VIGNAL <git@benoit-vignal.fr>
2025-03-14 11:03:07 +0100

    Improve default layout

--- components/FooterCopyright.vue
+++ components/FooterCopyright.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import {useAppConfigStore} from "~/stores/useAppConfig";
+  import dayjs from "dayjs";
 
   const props = defineProps({
     displayVersion: {
@@ -11,20 +12,54 @@
 
   const runtimeConfig = useRuntimeConfig()
   const appConfigStore = useAppConfigStore()
+
+  interface externalLink {
+    label: string,
+    to: string,
+  }
+
+  const versions: externalLink[] = [
+    {
+      label: `Version ${runtimeConfig.public.clientVersion}`,
+      to: 'https://github.com/froozeify/narvik-front'
+    }
+  ]
+  if (appConfigStore.config?.appVersion) {
+    versions.push({
+      label: `Backend ${appConfigStore.config.appVersion}`,
+      to: 'https://github.com/froozeify/narvik-back'
+    })
+  }
+
+  const externalLinks: externalLink[] = [
+    {
+      label: 'Documentation',
+      to: 'https://docs.narvik.app/frontend/'
+    }
+  ]
 </script>
 
 <template>
-  <div class="flex flex-row justify-center flex-wrap opacity-10 hover:opacity-30 gap-y-1 gap-x-4 text-xs print:hidden">
-    <div class="basis-full text-center"><a href="https://about.narvik.app/" target="_blank">© Benoît VIGNAL</a></div>
-
-    <template v-if="props.displayVersion">
-      <a href="https://github.com/froozeify/narvik-front" target="_blank">Version {{ runtimeConfig.public.clientVersion }}</a>-
-      <template v-if="appConfigStore.config && appConfigStore.config.appVersion">
-        <a href="https://github.com/froozeify/narvik-back" target="_blank">Backend {{ appConfigStore.config.appVersion }}</a>-
+  <div class="transition flex flex-row justify-center flex-wrap opacity-10 hover:opacity-30 gap-y-1 gap-x-4 text-xs print:hidden">
+    <div class="basis-full flex flex-wrap justify-center gap-2">
+      <template v-for="(val, index) in externalLinks">
+        <span v-if="index !== 0"> - </span>
+        <NuxtLink :to="val.to" target="_blank" >{{ val.label }}</NuxtLink>
       </template>
-    </template>
+    </div>
 
-    <a href="https://about.narvik.app/" target="_blank" class="underline">Narvik</a>
+    <div v-if="props.displayVersion" class="basis-full flex flex-wrap justify-center gap-2">
+      <template v-for="(val, index) in versions">
+        <span v-if="index !== 0"> - </span>
+        <NuxtLink :to="val.to" target="_blank" >{{ val.label }}</NuxtLink>
+      </template>
+    </div>
+
+    <a href="https://about.narvik.app/" target="_blank" class="basis-full flex flex-wrap justify-center gap-2">
+      <span class="underline">À propos de Narvik</span> -
+      <span >© 2024 - {{ dayjs().year() }}. Tous droits réservés</span>
+    </a>
+
   </div>
 </template>
 
