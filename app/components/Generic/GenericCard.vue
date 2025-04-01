97176166d0eea867fae1868af61891ea57ffe6f1
Benoit VIGNAL <git@benoit-vignal.fr>
2025-04-01 11:18:40 +0200

    Create generic card to have the default title styling

---
+++ app/components/Generic/GenericCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type {PropType} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false
+  }
+});
+
+</script>
+
+<template>
+  <UCard>
+    <div v-if="props.title" class="text-xl font-bold mb-4">{{ props.title }}</div>
+    <slot />
+  </UCard>
+</template>
+
+<style scoped lang="scss">
+
+</style>
