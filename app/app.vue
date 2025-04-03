46cfb20ada91fe97af7f0c4cfd03a48868377738
Benoit VIGNAL <git@benoit-vignal.fr>
2025-04-03 11:41:58 +0200

    Add legal acceptance check everywhere

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import ModalLegalsAcceptance from "~/components/Modal/ModalLegalsAcceptance";
+
 const appConfig = useAppConfig()
+
+const selfUser = useSelfUserStore()
+if (!selfUser.isLegalsAccepted()) {
+  useOverlay().create(ModalLegalsAcceptance).open()
+}
+
 </script>
 
 <template>
