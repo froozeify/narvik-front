38fe37f0b59a9f1864b8a79e77f9dc5642fe6247
Benoit VIGNAL <git@benoit-vignal.fr>
2025-04-07 18:53:28 +0200

    UI update

--- app/components/Generic/GenericStackedUpDown.vue
+++ app/components/Generic/GenericStackedUpDown.vue
@@ -14,12 +14,8 @@ const emit = defineEmits([
 
 <template>
   <div class="text-xs flex flex-col gap-0.5">
-    <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-      <UIcon name="i-heroicons-chevron-up" class="cursor-pointer" @click.stop="emit('changed', props.step)"/>
-    </div>
-    <div class="p-0.5 rounded-md text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 flex justify-center items-center">
-      <UIcon name="i-heroicons-chevron-down" class="cursor-pointer" @click.stop="emit('changed', - props.step)"/>
-    </div>
+    <UButton variant="soft" size="xs" icon="i-heroicons-chevron-up" @click.stop="emit('changed', props.step)" />
+    <UButton variant="soft" size="xs" icon="i-heroicons-chevron-down" @click.stop="emit('changed', - props.step)" />
   </div>
 </template>
 
