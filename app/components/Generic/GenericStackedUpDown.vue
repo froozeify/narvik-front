746e15657882cbc402b38cec486a136d92d37988
Benoit VIGNAL <git@benoit-vignal.fr>
2025-04-08 09:31:41 +0200

    Sale page update

--- app/components/Generic/GenericStackedUpDown.vue
+++ app/components/Generic/GenericStackedUpDown.vue
@@ -14,9 +14,12 @@ const emit = defineEmits([
 
 <template>
   <div class="text-xs flex flex-col gap-0.5">
-    <UButton variant="soft" size="xs" icon="i-heroicons-chevron-up" @click.stop="emit('changed', props.step)" />
-    <UButton variant="soft" size="xs" icon="i-heroicons-chevron-down" @click.stop="emit('changed', - props.step)" />
-  </div>
+    <div class="p-0.5 rounded-[calc(var(--ui-radius)*1.5)] font-small transition-colors cursor-pointer text-xs gap-1 text-(--ui-primary) bg-(--ui-primary)/10 hover:bg-(--ui-primary)/15 focus:outline-none focus-visible:bg-(--ui-primary)/15 flex justify-center items-center">
+      <UIcon name="i-heroicons-chevron-up" class="cursor-pointer" @click.stop="emit('changed', props.step)"/>
+    </div>
+    <div class="p-0.5 rounded-[calc(var(--ui-radius)*1.5)] font-small transition-colors cursor-pointer text-xs gap-1 text-(--ui-primary) bg-(--ui-primary)/10 hover:bg-(--ui-primary)/15 focus:outline-none focus-visible:bg-(--ui-primary)/15 flex justify-center items-center">
+      <UIcon name="i-heroicons-chevron-down" class="cursor-pointer" @click.stop="emit('changed', - props.step)"/>
+    </div>  </div>
 </template>
 
 <style scoped lang="css">
