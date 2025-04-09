47a4ab684da132a8db95f5ddf9150d76f820169c
Benoit VIGNAL <git@benoit-vignal.fr>
2025-04-09 09:28:51 +0200

    Update admin nav menu

--- app/components/Generic/Layout/GenericLayoutAdmin.vue
+++ app/components/Generic/Layout/GenericLayoutAdmin.vue
@@ -42,29 +42,12 @@ watchEffect(() => {
               <UNavigationMenu
                 orientation="vertical"
                 :items="groupedLinks.links"
-                @click="menuVisible = isDesktopDisplay"
-                :ui="{
-                  wrapper: 'border-s border-gray-200 dark:border-gray-800 space-y-2',
-                  base: 'group block border-s -ms-px xl:leading-6 before:hidden inline-flex',
-                  padding: 'p-0 ps-4',
-                  rounded: '',
-                  font: '',
-                  ring: '',
-                  active: 'text-primary-500 dark:text-primary-400 border-current font-semibold',
-                  inactive: 'border-transparent hover:border-gray-400 dark:hover:border-gray-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300',
-                  label: '',
-                  icon: {
-                    base: 'flex-shrink-0 w-4 h-4 mr-4',
-                    active: 'text-gray-700 dark:text-gray-200',
-                    inactive: 'text-gray-400 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200'
-                  }
-                }"
               />
             </template>
 
           </div>
           <div class="xl:basis-full">
-            <slot/>
+            <slot />
           </div>
         </div>
 
